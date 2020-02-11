a260c88ae42188173223b2e5156adbd8dbef8602
Wilbert Smith Antigua Castillo <wilbert_antigua@ucne.edu.do>
2020-02-10 18:29:02 -0600

    Entidad programa

---
+++ PrimerParcialAplicada/DAL/Contexto.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrimerParcialAplicada.DAL
+{
+    public class Contexto :DbContext
+    {
+        public Db
+    }
+}
---
+++ PrimerParcialAplicada/Entidades/Productos.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrimerParcialAplicada.Entidades
+{
+    public class Productos
+    {
+        public int ProductoId { get; set; }
+        public string  Descripcion { get; set; }
+        public int Existencia { get; set; }
+        public decimal Costo { get; set; }
+        public decimal ValorInventario { get; set; }
+
+        public Productos()
+        {
+            ProductoId = 0;
+            Descripcion = string.Empty;
+            Existencia = 0;
+            Costo = 0;
+            ValorInventario = 0;
+        }
+    }
+}
